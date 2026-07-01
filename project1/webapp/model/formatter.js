345c6e96b1e8fd0dc36cab073f468529500ce359
Force <maxfunmaxfun672@gmail.com>
2026-07-01 22:12:03 +0300

    add published year formatter and update Main view to use it

---
+++ project1/webapp/model/formatter.js
@@ -0,0 +1,10 @@
+sap.ui.define([], function () {
+  "use strict";
+
+  return {
+    publishedYear(date) {
+      const resourceBundle = this?.getOwnerComponent()?.getModel("i18n")?.getResourceBundle();
+      return resourceBundle.getText("publishedYear", date.split("-")[0]);
+    },
+  };
+});
