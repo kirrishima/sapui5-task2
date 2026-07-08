1baff0ebf036fb19496fbabb3d393fedbc85960f
Force <maxfunmaxfun672@gmail.com>
2026-07-08 23:08:37 +0300

    enhance product view with detailed layout and supplier information

--- project1/webapp/controller/Product.controller.js
+++ project1/webapp/controller/Product.controller.js
@@ -1,5 +1,40 @@
-sap.ui.define(["./BaseController"], (BaseController) => {
+sap.ui.define(["./BaseController", "sap/ui/model/json/JSONModel"], (BaseController, JSONModel) => {
   "use strict";
 
-  return BaseController.extend("project1.controller.Product", { onInit() {} });
+  return BaseController.extend("project1.controller.Product", {
+    onInit() {
+      const viewModel = new JSONModel({});
+      this.getView().setModel(viewModel, "view");
+
+      this._viewModel = viewModel;
+      this._v2Model = this.getOwnerComponent().getModel("v2Model");
+      this.getOwnerComponent().getRouter().getRoute("RouteProduct").attachMatched(this._onProductRouteMatched, this);
+    },
+
+    async _onProductRouteMatched(event) {
+      const productID = event.getParameter("arguments").ProductID;
+
+      if (productID == null) {
+        return;
+      }
+
+      await this._v2Model.metadataLoaded();
+
+      const productPath = this._v2Model.createKey("/Products", {
+        ID: Number(productID),
+      });
+
+      this._v2Model.read(productPath, {
+        urlParameters: {
+          $expand: "Supplier",
+        },
+        success: (product) => {
+          this._viewModel.setData(product);
+        },
+        error: () => {
+          this._viewModel.setData({});
+        },
+      });
+    },
+  });
 });
